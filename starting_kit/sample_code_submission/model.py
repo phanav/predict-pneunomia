8e80fcdaa677cddef06327a01635b2e29c0a7fa9
phanav <phanav@github.com>
2021-02-03 19:49:13 +0100

    Update readme.ipynb model.py

--- starting_kit/sample_code_submission/model.py
+++ starting_kit/sample_code_submission/model.py
@@ -10,17 +10,21 @@ import pickle
 import numpy as np   # We recommend to use numpy arrays
 from os.path import isfile
 from sklearn.base import BaseEstimator
+from sklearn.ensemble import RandomForestClassifier
 
-class model (BaseEstimator):
-    def __init__(self):
+class model(BaseEstimator):
+    def __init__(self, depth=5):
         '''
         This constructor is supposed to initialize data members.
         Use triple quotes for function documentation. 
         '''
-        self.num_train_samples=0
-        self.num_feat=1
-        self.num_labels=1
+#         self.num_train_samples=0
+#         self.num_feat=1
+#         self.num_labels=1
         self.is_trained=False
+        
+        self.depth=depth
+        self.learner = RandomForestClassifier(max_depth=self.depth)
 
     def fit(self, X, y):
         '''
@@ -36,16 +40,18 @@ class model (BaseEstimator):
         Use data_converter.convert_to_num() to convert to the category number format.
         For regression, labels are continuous values.
         '''
-        self.num_train_samples = X.shape[0]
-        if X.ndim>1: self.num_feat = X.shape[1]
-        print("FIT: dim(X)= [{:d}, {:d}]".format(self.num_train_samples, self.num_feat))
-        num_train_samples = y.shape[0]
-        if y.ndim>1: self.num_labels = y.shape[1]
-        print("FIT: dim(y)= [{:d}, {:d}]".format(num_train_samples, self.num_labels))
-        if (self.num_train_samples != num_train_samples):
-            print("ARRGH: number of samples in X and y do not match!")
+#         self.num_train_samples = X.shape[0]
+#         if X.ndim>1: self.num_feat = X.shape[1]
+#         print("FIT: dim(X)= [{:d}, {:d}]".format(self.num_train_samples, self.num_feat))
+#         num_train_samples = y.shape[0]
+#         if y.ndim>1: self.num_labels = y.shape[1]
+#         print("FIT: dim(y)= [{:d}, {:d}]".format(num_train_samples, self.num_labels))
+#         if (self.num_train_samples != num_train_samples):
+#             print("ARRGH: number of samples in X and y do not match!")
+        self.learner.fit(X, y)
         self.is_trained=True
-
+        return self.learner
+        
     def predict(self, X):
         '''
         This function should provide predictions of labels on (test) data.
@@ -58,15 +64,16 @@ class model (BaseEstimator):
         Scikit-learn also has a function predict-proba, we do not require it.
         The function predict eventually can return probabilities.
         '''
-        num_test_samples = X.shape[0]
-        if X.ndim>1: num_feat = X.shape[1]
-        print("PREDICT: dim(X)= [{:d}, {:d}]".format(num_test_samples, num_feat))
-        if (self.num_feat != num_feat):
-            print("ARRGH: number of features in X does not match training data!")
-        print("PREDICT: dim(y)= [{:d}, {:d}]".format(num_test_samples, self.num_labels))
-        y = np.zeros([num_test_samples, self.num_labels])
+#         num_test_samples = X.shape[0]
+#         if X.ndim>1: num_feat = X.shape[1]
+#         print("PREDICT: dim(X)= [{:d}, {:d}]".format(num_test_samples, num_feat))
+#         if (self.num_feat != num_feat):
+#             print("ARRGH: number of features in X does not match training data!")
+#         print("PREDICT: dim(y)= [{:d}, {:d}]".format(num_test_samples, self.num_labels))
+#         y = np.zeros([num_test_samples, self.num_labels])
         # If you uncomment the next line, you get pretty good results for the Iris data :-)
         #y = np.round(X[:,3])
+        y = self.learner.predict(X)
         return y
 
     def save(self, path="./"):
